e367613377cdc1e5aed17f742c1413ff2a5c3405
brunohaf <31559311+brunohaf@users.noreply.github.com>
2024-01-20 22:44:31 -0300

    chore: cleans redundant code on main.py

--- main.py
+++ main.py
@@ -2,12 +2,9 @@ from typing import Union
 
 from fastapi import FastAPI
 
-from fastapi import FastAPI
 from routers import url_shortener_router, users_router
 
-# uvicorn main.todos.main:app --reload
-
-app = FastAPI(root_path="/api", title="URL Shortener API", description="API for URL Shortener", version="1.0.0", swagger_ui_init_oauth={"clientId": "swagger-ui-init-oauth"})
+app = FastAPI(root_path="/api", title="URL Shortener API", description="API for URL Shortener", version="1.0.0")
 
 app.include_router(url_shortener_router.router)
 app.include_router(users_router.router)
