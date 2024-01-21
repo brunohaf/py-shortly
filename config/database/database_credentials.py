01534a7df7352f68f77c8e440aab878c78056a42
brunohaf <31559311+brunohaf@users.noreply.github.com>
2024-01-20 22:35:42 -0300

    chore: remove mockup conn str from database_credentials.py

--- config/database/database_credentials.py
+++ config/database/database_credentials.py
@@ -2,5 +2,3 @@ class DatabaseCredentials():
     def __init__(self):
         self.connection_str = ''
         self.conn_args = {'check_same_thread': False}
-
-# mysql+pymysql://myuser:mypassword@172.17.0.2/Shortly
