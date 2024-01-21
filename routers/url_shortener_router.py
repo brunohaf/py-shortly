2f3adce8fc8e1a6ba56fdd43c86df27633aca574
brunohaf <bruno3145@gmail.com>
2024-01-21 15:26:58 -0800

    feat: jwt authN flow for url listing

--- routers/url_shortener_router.py
+++ routers/url_shortener_router.py
@@ -1,19 +1,26 @@
-from fastapi import APIRouter, Depends
-from fastapi.responses import RedirectResponse
+from fastapi import APIRouter, Depends, status
+from fastapi.responses import RedirectResponse, Response
 from entities.models.url import UrlRequest
+from services import url_shortener_service
+from typing import List
 
 from services import url_shortener_service
 
 router = APIRouter(
-    prefix="/u",
     tags=["url_shortener"]
 )
 
 @router.get("/{url_id}")
-async def redirect(url_id: str, long_url = Depends(url_shortener_service.get_long_url)):
+async def redirect(url_id: str, long_url = Depends(url_shortener_service.get_long_url)) -> RedirectResponse:
     redirect = long_url.original_url 
     return RedirectResponse(url = redirect, status_code=302)
 
-@router.post("/shorten")
-async def shorten(url: UrlRequest, shortened = Depends(url_shortener_service.shorten)):
+@router.post("/api/shorten", status_code=status.HTTP_201_CREATED)
+async def shorten(url: UrlRequest, shortened = Depends(url_shortener_service.shorten)) -> str:
     return shortened
+
+@router.get("/api/shortens")
+async def get_shortens(shortens: List[str] = Depends(url_shortener_service.get_user_urls)):
+    if len(shortens) == 0:
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+    return shortens
