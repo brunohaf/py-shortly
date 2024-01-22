7fc9c5f4f7bd7f53a7b050d66c4a90097d71d90a
brunohaf <bruno3145@gmail.com>
2024-01-22 08:05:26 -0800

    feat: pass hash service tests

--- app/services/tests/password_hash_service_test.py
+++ app/services/tests/password_hash_service_test.py
@@ -1,21 +1,22 @@
 from services import password_hash_service as pwd_service
+from pytest import fixture
 
-# Arrange
-PASSWORD = "SuperComplexPassword!"
+@fixture
+def password() -> str:
+    return "SuperComplexPassword!"
 
-def test_get_password_hash_should_be_different_from_original_password():
+def test_get_password_hash_should_be_different_from_original_password(password):
 
     # Act
-    hashed_password = pwd_service.get_password_hash(PASSWORD)
+    hashed_password = pwd_service.get_password_hash(password)
 
     # Assert
-    assert hashed_password != PASSWORD
+    assert hashed_password != password
 
-def test_get_password_hash_alg_should_be_bcrypt():
-
+def test_get_password_hash_alg_should_be_valid(password):
 
     # Arrange
-    hashd = pwd_service.get_password_hash(PASSWORD)
+    hashd = pwd_service.get_password_hash(password)
 
     # Act
     alg = pwd_service.pwd_context.identify(hashd)
@@ -23,13 +24,13 @@ def test_get_password_hash_alg_should_be_bcrypt():
     # Assert
     assert alg in pwd_service.schemes
 
-def test_verify_password_should_be_valid():
+def test_verify_password_should_be_valid(password):
 
     # Arrange
-    hashd = pwd_service.get_password_hash(PASSWORD)
+    hashd = pwd_service.get_password_hash(password)
 
     # Act
-    is_valid_password = pwd_service.verify_password(PASSWORD, hashd)
+    is_valid_password = pwd_service.verify_password(password, hashd)
 
     # Assert
     assert is_valid_password is True
